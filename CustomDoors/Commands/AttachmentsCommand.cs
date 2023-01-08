111aadf913f20f5873fbb14da21ac08b3f31d427
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2023-01-08 21:40:25 +0100

    1.1.0

---
+++ CustomDoors/Commands/AttachmentsCommand.cs
@@ -0,0 +1,52 @@
+﻿using CommandSystem;
+using PluginAPI.Core;
+using System;
+using InventorySystem.Items;
+using InventorySystem.Items.Firearms;
+
+namespace CustomDoors.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class AttachmentsCommand : ICommand
+    {
+        public string Command { get; } = "Attachments";
+
+        public string[] Aliases { get; } = new string[] { };
+
+        public string Description { get; } = "Display your weapon's attachments value.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            ItemBase itemBase = Player.Get(((CommandSender)sender).SenderId).CurrentItem;
+
+            response = "Error.";
+            bool allow = false;
+
+
+            if (itemBase is not Firearm firearm)
+                response = "You're not holding a weapon.";
+
+
+            /*else if (arguments.Count > 0) forceattachments exists actually
+            {
+                if (uint.TryParse(arguments.FirstOrDefault(), out uint attachment))
+                {
+                    firearm.Status = new FirearmStatus(firearm.Status.Ammo, firearm.Status.Flags, attachment);
+                    allow = true;
+                }
+                else
+                    response = "The argument must be a number (uint).";
+            }*/
+
+            else
+            {
+                response = firearm.Status.Attachments.ToString();
+                allow = true;
+            }
+
+            if (PluginClass.Singleton.config.InfoAttachmentsCommand)
+                Log.Info(response);
+            return allow;
+        }
+    }
+}
--- CustomDoors/EventHandler.cs
+++ CustomDoors/EventHandler.cs
@@ -6,6 +6,13 @@ using Interactables.Interobjects.DoorUtils;
 using System.Linq;
 using MapGeneration;
 using System;
+using PluginAPI.Core;
+using InventorySystem;
+using InventorySystem.Items.Pickups;
+using InventorySystem.Items.Firearms;
+using System.Collections.Generic;
+using InventorySystem.Items.Firearms.Ammo;
+using InventorySystem.Items;
 
 namespace CustomDoors
 {
@@ -14,34 +21,59 @@ namespace CustomDoors
         [PluginEvent(ServerEventType.WaitingForPlayers)]
         public void WaitingForPlayers()
         {
-            foreach (DoorConfig doorConfig in PluginClass.Singleton.config.Doors)
+            List<SpawnableConfig>  spawnableObjects = new(PluginClass.Singleton.config.Doors);
+            spawnableObjects.AddRange(PluginClass.Singleton.config.Items);
+
+            //foreach (SpawnableConfig spawnableConfig in PluginClass.Singleton.config.SpawnableObjects) bugged
+            foreach (SpawnableConfig spawnableConfig in spawnableObjects)
             {
-                RoomIdentifier room = RoomIdentifier.AllRoomIdentifiers.FirstOrDefault(r => r.name == doorConfig.RoomName);
-                
+                RoomIdentifier room = RoomIdentifier.AllRoomIdentifiers.FirstOrDefault(r => r.name == spawnableConfig.RoomName);
+
                 if (room == null)
                     continue;
-                
-                Vector3 scale = new(doorConfig.ScaleX, doorConfig.ScaleY, doorConfig.ScaleZ);
 
-                float roomRotationDiffY =  doorConfig.defaultRoomRotationYAxis - room.transform.rotation.eulerAngles.y;
-                
-                Vector3 doorRotation = new(doorConfig.RotationX, doorConfig.RotationY + roomRotationDiffY, doorConfig.RotationZ);
+                Vector3 scale = new(spawnableConfig.ScaleX, spawnableConfig.ScaleY, spawnableConfig.ScaleZ);
 
-                Vector3 relativePosition = new(doorConfig.RelativePositionX, doorConfig.RelativePositionY, doorConfig.RelativePositionZ);
+                float roomRotationDiffY = spawnableConfig.DefaultRoomRotationYAxis - room.transform.rotation.eulerAngles.y;
 
-                Vector3 rotatedRelativePosition = new(
-                    relativePosition.x * (float)Math.Cos(roomRotationDiffY * Math.PI / 180) - relativePosition.z * (float)Math.Sin(roomRotationDiffY * Math.PI / 180), // Math.PI / 180 : convert angle to radian
-                    relativePosition.y,
-                    relativePosition.z * (float)Math.Cos(roomRotationDiffY * Math.PI / 180) + relativePosition.x * (float)Math.Sin(roomRotationDiffY * Math.PI / 180)
-                    );
+                Vector3 objectRotation = new(spawnableConfig.RelativeRotationX, spawnableConfig.RelativeRotationY + roomRotationDiffY, spawnableConfig.RelativeRotationZ);
 
-                DoorVariant door = UnityEngine.Object.Instantiate(UnityEngine.Object.FindObjectsOfType<DoorSpawnpoint>().First(d => d.TargetPrefab.name.Contains(doorConfig.Type)).TargetPrefab, room.transform.position + rotatedRelativePosition,  Quaternion.Euler(doorRotation));
-                door.transform.localScale = scale;
-                door.RequiredPermissions.RequiredPermissions = doorConfig.KeycardPermissions;
-                NetworkServer.Spawn(door.gameObject);
-                door.ServerChangeLock(DoorLockReason.SpecialDoorFeature, doorConfig.Locked);
-                door.TargetState = doorConfig.Opened;
+                Vector3 finalPosition = GetRotatedPosition(roomRotationDiffY, new(spawnableConfig.RelativePositionX, spawnableConfig.RelativePositionY, spawnableConfig.RelativePositionZ)) + room.transform.position;
+
+                if (spawnableConfig is DoorConfig doorConfig)
+                {
+                    DoorVariant door = UnityEngine.Object.Instantiate(UnityEngine.Object.FindObjectsOfType<DoorSpawnpoint>().First(d => d.TargetPrefab.name.Contains(doorConfig.Type)).TargetPrefab, finalPosition, Quaternion.Euler(objectRotation));
+                    door.transform.localScale = scale;
+                    door.RequiredPermissions.RequiredPermissions = doorConfig.KeycardPermissions;
+                    NetworkServer.Spawn(door.gameObject);
+                    door.ServerChangeLock(DoorLockReason.SpecialDoorFeature, doorConfig.Locked);
+                    door.TargetState = doorConfig.Opened;
+                }
+
+                else if (spawnableConfig is ItemConfig itemConfig)
+                {
+                    ItemBase itemBase = ReferenceHub.HostHub.inventory.ServerAddItem(itemConfig.Item);
+
+                    ItemPickupBase itemPickup = itemBase.ServerDropItem();
+
+                    itemPickup.transform.position = finalPosition;
+                    itemPickup.transform.rotation = Quaternion.Euler(objectRotation);
+                    itemPickup.transform.localScale = scale;
+
+                    if (itemPickup is AmmoPickup ammoPickup)
+                        ammoPickup.NetworkSavedAmmo = itemConfig.Ammo;
+
+                    else if (itemPickup is FirearmPickup firearmPickup)
+                        firearmPickup.Status = new FirearmStatus((byte)itemConfig.Ammo, itemConfig.Ammo == 0 ? FirearmStatusFlags.None : FirearmStatusFlags.MagazineInserted, itemConfig.Attachments);
+                }
             }
         }
+
+        public Vector3 GetRotatedPosition(float rotationDiffY, Vector3 originalPosition) =>
+        new(
+            originalPosition.x * (float)Math.Cos(rotationDiffY * Math.PI / 180) - originalPosition.z * (float)Math.Sin(rotationDiffY * Math.PI / 180), // Math.PI / 180 : converts angle to radian
+            originalPosition.y,
+            originalPosition.z * (float)Math.Cos(rotationDiffY * Math.PI / 180) + originalPosition.x * (float)Math.Sin(rotationDiffY * Math.PI / 180)
+        );
     }
 }
